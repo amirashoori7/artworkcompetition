74c5e829f3dcdd2bd054d538b60a7f2b33d620dd
amirashoori7 <a.ashoori7@gmail.com>
2019-12-24 11:35:21 +0330

    authentication backend bug fixed, now login possible with email and/or username, tested and ok!

--- account/backends.py
+++ account/backends.py
@@ -1,17 +1,24 @@
 from .models import ProjectUser
-from django.contrib.auth.models import check_password
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+#from django.contrib.auth.models import check_password
 
-class EmailAuthBackend(object):
-    def authenticate(self, username=None, password=None):
+class EmailAuthBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        User = get_user_model()
         try:
-            user = ProjectUser.objects.get(email=username)
+            user = User.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+        except User.DoesNotExist:
+            return None
+        else:
             if user.check_password(password):
                 return user
-            except ProjectUser.DoesNotExist:
-                return None
+        return None
 
     def get_user(self, user_id):
+        User = get_user_model()
         try:
-            return ProjectUser.objects.get(pk=user_id)
-        except ProjectUser.DoesNotExist:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
             return None
