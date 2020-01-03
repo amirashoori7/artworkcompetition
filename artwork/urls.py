f29689375690af43983c70076b0583a127a45161
amirashoori7 <a.ashoori7@gmail.com>
2020-01-03 22:03:46 +0330

    dashindex view added

--- artwork/urls.py
+++ artwork/urls.py
@@ -6,6 +6,7 @@ app_name = 'artwork'
 
 urlpatterns = [
     #re_path(r'^index/$', views.index, name='index'),
+    path('dashindex', views.dashindex, name='dashindex'),
     path('rest_list', rest_views.worklists),
     path('', views.index, name='index'),
     path('entry_form/', views.entry_form, name='entry_form'),
