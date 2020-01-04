ff3ba1fa7831e937d2fceca36d3e862be1f75efd
Salehzadeh <s213457989@Mandela.ac.za>
2020-01-04 14:35:19 +0200

    ccc

--- artwork/models.py
+++ artwork/models.py
@@ -1,18 +1,32 @@
 from django.db import models
-from dateutil.relativedelta import relativedelta
-from datetime import *
-from django.urls import reverse
+from django.template.defaultfilters import default
+import os
+from uuid import uuid4
 
 
 class School(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     address = models.CharField(max_length=500)
+    province = models.IntegerField(default=0,blank=False,null=False)
+    
     class Meta:
         ordering = ('name',)
 
     def __str__(self):
         return self.name
 
+def path_and_rename(path):
+    def wrapper(instance, filename):
+        ext = filename.split('.')[-1]
+        # get filename
+        if instance.pk:
+            filename = '{}.{}'.format(instance.pk, ext)
+        else:
+            # set filename as random string
+            filename = '{}.{}'.format(uuid4().hex, ext)
+        # return the whole path to the file
+        return os.path.join(path, filename)
+    return wrapper
 
 class Artwork(models.Model):
     firstname = models.CharField(max_length=200)
@@ -24,13 +38,15 @@ class Artwork(models.Model):
     bioapproved = models.BooleanField(default=False)
     parentname = models.CharField(blank=True, max_length=200)
     parentphone = models.CharField(blank=True, null=True, max_length=100)
-    parentemail = models.CharField(blank=True, null=True, max_length=100)
-    school = models.CharField(max_length=100)
+    parentemail = models.CharField(blank=False, null=False, max_length=100)
+    school = models.CharField(blank=False, null=False, max_length=100)
     teachername = models.CharField(blank=True, max_length=300)
-    teacherphone = models.CharField(blank=True, null=True, max_length=100)
+    teacherphone = models.CharField(blank=False, null=True, max_length=100)
     teacheremail = models.CharField(blank=False, null=True, max_length=100)
     worktitle = models.CharField(blank=False, null=True, max_length=300)
     workfile = models.FileField(upload_to='works')
+    workfileCropped = models.TextField(blank=False, default="", null=False)
+    workformulafile = models.FileField(null=True, blank='True',upload_to='formula')
     workapproved = models.BooleanField(default=False)
     question1 = models.TextField(blank=False)
     question2 = models.TextField(blank=False)
--- static/js/math.js
+++ static/js/math.js
@@ -2,7 +2,7 @@ var tweenMenuShow
 function windowLayoutFitSize() {
 	$("#main-body").width($(window).width())
 	$("#main-body").height($(window).height())
-	var frameHeight = ($(window).height() * .85)
+	var frameHeight = ($(window).height() * .95)
 	var isHorizontal = true;
 	var frameWidth = frameHeight + (frameHeight * 13 / 21)
 	$(".page-content").width(frameWidth)
@@ -22,15 +22,16 @@ function loadContent(liItem) {
 		TweenMax.fromTo(".page-content", 1, { scaleY: 0, scaleX: 0, rotation: -180 },
 			{ 
 			scaleY: 1, 
-			scaleX: 1, 
-			rotation: 0, 
-			left: ($(window).width() - $(".page-content").width()) / 2, 
-			top: ($(window).height() - $(".page-content").height()) / 2 + "px", 
-			transformOrigin: "center" 
+			scaleX: 1,
+			rotation: 0,
+			left: ($(window).width() - $(".page-content").width()) / 2,
+			top: ($(window).height() - $(".page-content").height()) / 2 + "px",
+			transformOrigin: "center"
 		})
 			
 	})
 }
+
 $(window).bind('resize', function () {
 	windowLayoutFitSize()
 });
@@ -64,151 +65,6 @@ $(window).on("load", function () {
 	toggleMessageBox(null, null)
 })
 
-function loadBackgroundAnimParticles() {
-	var canvas, ctx, w, h;
-
-	var h = $(window).height()
-	var w = $(window).width()
-
-	var canvas = document.createElement('canvas')
-	document.body.appendChild(canvas)
-	canvas.width = w
-	canvas.height = h
-	ctx = canvas.getContext('2d')
-
-	function paintCanvas() {
-		ctx.fillStyle = "rgba(22,77,122,1)"
-		ctx.fillRect(0, 0, w, h)
-	}
-
-
-	function particle(r, off, c) {
-		this.x = Math.random() * w
-		this.y = Math.random() * h
-		this.r = r
-		this.offset = Math.random() * 50 + off
-		this.color = "rgba(255,255,255," + c + ")"
-		this.draw = function () {
-			ctx.fillStyle = this.color
-			ctx.beginPath()
-			ctx.arc(this.x, this.y, this.r, Math.PI * 2, false)
-			ctx.fill()
-		}
-
-	}
-
-	var layer_1 = [],
-		layer_2 = [],
-		layer_3 = [];
-
-	var layer_1_num = 33
-	var layer_2_num = 66
-	var layer_3_num = 99
-
-
-	for (i = 0; i < layer_1_num; i++) {
-		layer_1.push(new particle(4, 11, 1))
-
-	}
-	for (i = 0; i < layer_2_num; i++) {
-		layer_2.push(new particle(2.5, 33, 1))
-	}
-	for (i = 0; i < layer_3_num; i++) {
-		layer_3.push(new particle(1, 66, 1))
-	}
-
-	function draw() {
-		for (i = 0; i < layer_1.length; i++) {
-			var p = layer_1[i]
-			p.draw()
-			update(p)
-			checkBounds(p)
-			for (var j = i + 1; j < layer_3.length; j++) {
-				p2 = layer_3[j]
-				distance(p, p2, w * 1.5)
-			}
-
-		}
-		for (i = 0; i < layer_2.length; i++) {
-			var p = layer_2[i];
-			p.draw();
-			update(p);
-			checkBounds(p);
-			for (var j = i + 1; j < layer_3.length; j++) {
-				p2 = layer_3[j];
-				distance(p, p2);
-			}
-		}
-		for (i = 0; i < layer_3.length; i++) {
-			var p = layer_3[i];
-			p.draw();
-			update(p);
-			checkBounds(p);
-			for (var j = i + 1; j < layer_3.length; j++) {
-				p2 = layer_3[j];
-				distance(p, p2);
-			}
-		}
-	}
-	function update(p) {
-		p.x = p.x - mouse.x / p.offset;
-		p.y = p.y - mouse.y / p.offset;
-	}
-	function checkBounds(p) {
-		if (p.x > w) {
-			p.x = 0;
-		}
-		else if (p.x < 0) {
-			p.x = w;
-		}
-		if (p.y > h) {
-			p.y = 0;
-		}
-		else if (p.y < 0) {
-			p.y = h;
-		}
-	}
-
-	var minDist = w * 0.7;
-
-	function distance(p1, p2) {
-		var dist;
-		var dx = p1.x - p2.x;
-		var dy = p1.y - p2.y;
-		dist = Math.sqrt(dx * dx + dy * dy);
-
-		if (dist <= minDist) {
-			ctx.beginPath();
-			ctx.strokeStyle = "rgba(255,255,255," + (0.2 - dist / minDist) + ")";
-			ctx.moveTo(p1.x, p1.y);
-			ctx.lineTo(p2.x, p2.y);
-			ctx.stroke();
-			ctx.closePath();
-		}
-	}
-
-	var mouse = {
-		x: 0,
-		y: 0
-	}
-
-	document.addEventListener('mousemove', function (e) {
-		mouse.x = e.clientX - w / 2 || e.pageX - w / 2;
-		mouse.y = e.clientY - h / 2 || e.pageY - h / 2;
-	}, false);
-
-	function animate() {
-		paintCanvas();
-		draw();
-		requestAnimationFrame(animate);
-	}
-	// setInterval(function(){
-	// paintCanvas();
-	// draw();
-	// },30);
-	animate();
-}
-
 
 function loadUniverseBG() {
 	window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
@@ -223,8 +79,8 @@ function loadUniverseBG() {
 	var circleCenter;
 	var first = true;
 	var cometColor = '180,184,240';
-	var giantColor = '226,225,142';
-	var starColor = '226,225,224';
+	var giantColor = '160,134,240';
+	var starColor = '170,114,240';
 	var canva = document.getElementById('universe');
 	var stars = [];
 
@@ -273,7 +129,7 @@ function loadUniverseBG() {
 			this.fadingOut = null;
 			this.fadingIn = true;
 			this.opacity = 0;
-			this.opacityTresh = getRandInterval(.2, 1 - (this.comet + 1 - 1) * .4);
+			this.opacityTresh = getRandInterval(.2, 1 - (this.comet + 1 - 1) * .8);
 			this.do = getRandInterval(0.0005, 0.002) + (this.comet + 1 - 1) * .001;
 		};
 
@@ -286,7 +142,7 @@ function loadUniverseBG() {
 
 		this.fadeOut = function () {
 			if (this.fadingOut) {
-				this.fadingOut = this.opacity < 0 ? false : true;
+				this.fadingOut = this.2 < 0 ? false : true;
 				this.opacity -= this.do / 2;
 				if (this.x > width || this.y < 0) {
 					this.fadingOut = false;
@@ -382,11 +238,13 @@ function filterFunction() {
 }
 
 
+//SHOWS/HIDES THE MESSAGE BOX, 
+// INPUT: 1- gets a text message and boolean (True pops the error message. False pops the success message)
 var messageBoxTween;
 function toggleMessageBox(messageText, isError) {
 	var tl = new TimelineLite({paused: true})
 	if(messageText == null || messageBoxTween==null) {
-// THE FIRST TOGGLE AND WHEN BOX IS HIDDEN
+		// THE TOGGLE WHILE DOCUMENT LOADING AND WHEN BOX SHOULD BE HIDDEN
 		$(".error-message-box").html("")
 		$(".success-message-box").html("")
 		if(messageBoxTween != null && !messageBoxTween.reversed()){
