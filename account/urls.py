3078dbfaa9cf003d228f60b6247c0347330770c5
amirashoori7 <a.ashoori7@gmail.com>
2020-01-08 03:47:33 +0330

    userlist view and coresponding url path is added, i have used django built in generic views, plz try to test it in your template, if didnt work inform me, maybe you might have to add template name in urls.py

--- account/urls.py
+++ account/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     #path('userlogin/', views.userlogin, name='userlogin'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
+    path('userlist', ProjectUserList.as_view()),
 ]
