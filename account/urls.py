45b99adcee42f23f39b194653128b873fc2488ce
amirashoori7 <a.ashoori7@gmail.com>
2020-01-25 02:22:12 +0330

    registration template and url config added

--- account/urls.py
+++ account/urls.py
@@ -7,7 +7,8 @@ app_name = 'account'
 
 urlpatterns = [
     #path('userlogin/', views.userlogin, name='userlogin'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/' views.registration, name='register'),
     path('userlist', ProjectUserList.as_view()),
 ]
