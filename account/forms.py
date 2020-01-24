13f09c98bb495b765a31ffeac06202ab08431efd
amirashoori7 <a.ashoori7@gmail.com>
2020-01-25 01:28:11 +0330

    user registration with email username and pass

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,15 @@
+from . models import ProjectUser
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(UserCreationForm):
+    username = forms.CharField(max_length=30)
+    email = forms.EmailField(max_length=100)
+
+    class Meta:
+        model = ProjectUser
+        fields = ('username', 'email', 'password1', 'password2')
--- account/views.py
+++ account/views.py
@@ -1,5 +1,16 @@
 from django.views.generic import ListView
 from .models import ProjectUser
+from .forms import UserRegistrationForm
 
 class ProjectUserList(ListView):
     model = ProjectUser
+
+def registration(request):
+    form = UserRegistrationForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        return render(request, 'register.html', {'user': user})
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'register.html', context)
