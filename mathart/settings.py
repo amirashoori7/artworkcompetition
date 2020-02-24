78defb9118fc3c0f43e72fc78d6163f54d4f67c9
Salehzadeh <s213457989@Mandela.ac.za>
2020-02-24 08:30:22 +0200

    vvvv

--- mathart/settings.py
+++ mathart/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'mathart',
-    'USER': 'mathartuse',
+    'USER': 'mathartuser',
     'PASSWORD': 'mathartuser',
     'HOST':    'localhost',
     'PORT': '',
